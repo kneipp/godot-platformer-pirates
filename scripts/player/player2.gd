14440cac19c4ee24d6926fc36c905259ce0c1a58
Fabrício Kneipp <fabricio.kneipp@syngenta.com>
2025-04-24 21:20:18 -0300

    feat: add player2

--- scripts/player/player2.gd
+++ scripts/player/player2.gd
@@ -14,18 +14,18 @@ var direction :
 		sprite.flip_h = value == -1
 
 func _physics_process(delta: float) -> void:
-	var x_input = Input.get_axis("btn_left", "btn_right")	
+	var x_input = Input.get_axis("p2_btn_left", "p2_btn_right")	
 	direction = x_input
 	
-	if Input.is_action_just_pressed("btn_jump") and is_on_floor():
+	if Input.is_action_just_pressed("p2_btn_jump") and is_on_floor():
 		velocity.y = JUMP_FORCE
-		sprite.play("p1_jump")
+		sprite.play("p2_jump")
 	elif not is_on_floor() and velocity.y >= 0:
-		sprite.play("p1_fall")
+		sprite.play("p2_fall")
 	elif x_input == 0 and is_on_floor():
-		sprite.play("p1_idle")
+		sprite.play("p2_idle")
 	else:
-		sprite.play("p1_run")
+		sprite.play("p2_run")
 		
 	velocity.x = x_input * 90
 	velocity.y += GRAVITY * delta
